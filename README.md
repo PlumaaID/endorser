1117e0ad9210343075ddacffad20ff4c48b63a9c
ernestognw <ernestognw@gmail.com>
2024-05-15 18:39:20 -0600

    Add Arbitrum Sepolia deployment

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 A contract to track digital endorsements according to comercial Mexican law. This contract allows for any user to mint a new token by providing the hash of the digital document that's endorsed. Privacy is ensured by only tracking the hash of the document, and not the document itself.
 
+## Deployments
+
+| Network          | Endorser Proxy                             |
+| ---------------- | ------------------------------------------ |
+| Arbitrum Sepolia | 0xf391CE6C3467E21CeF7154c008B2d4520EA8e953 |
+
 ## Installing Foundry
 
 See [Foundry installation guide](https://book.getfoundry.sh/getting-started/installation).
--- script/Endorser.s.sol
+++ script/Endorser.s.sol
@@ -4,17 +4,33 @@ pragma solidity ^0.8.20;
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 import {Endorser} from "~/Endorser.sol";
-import {Upgrades} from "@openzeppelin/foundry-upgrades/Upgrades.sol";
+import {ICreateX} from "createx/ICreateX.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract EndorserScript is Script {
-    function setUp() public {}
+    address constant CREATE_X = 0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed;
+    ICreateX public createX;
+
+    function setUp() public {
+        createX = ICreateX(CREATE_X);
+    }
 
     function run() public {
         vm.startBroadcast();
-        address initialOwner = address(0); // Replace
-        address proxy = Upgrades.deployUUPSProxy(
-            "Endorser.sol",
-            abi.encodeCall(Endorser.initialize, (initialOwner))
+
+        address implementation = createX.deployCreate2(
+            keccak256("ENDORSER_IMPLEMENTATION_V1"),
+            type(Endorser).creationCode
+        );
+        address proxy = createX.deployCreate2(
+            keccak256("ENDORSER_ERC1967PROXY"),
+            abi.encodePacked(
+                type(ERC1967Proxy).creationCode,
+                abi.encode(
+                    implementation,
+                    abi.encodeCall(Endorser.initialize, (msg.sender))
+                )
+            )
         );
         console.log("Contract deployed to %s", address(proxy));
         vm.stopBroadcast();
